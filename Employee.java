060be07ede73d4b4a3e8f8db2d3d1e6fe38c0ac8
Victor Nguyen <vdn140030@gmail.com>
2016-12-13 15:00:22 -0600

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,14 @@
+/***************************************
+ * This Employee class defines the basic
+ * data fields and methods for storing
+ * information. Instances of this class
+ * can be created.
+ *
+ * Created by:  Victor on 12/8/2016.
+ *              vdn140030
+ *              CS 2336.005
+ *              FALL 2016
+ **************************************/
+// All methods and data fields are defined in the
+// abstract class.
+class Employee extends abstractPerson {}
